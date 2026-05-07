3712e68358df6ac2a4ff6bb96288b25f83fc138e
RahulXTech <rahul.code2024@gmail.com>
2026-05-07 21:58:09 +0530

    Implement business profile module with routes and controller updates

---
+++ backend/models/bussinessProfileModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { isLowercase, trim } from "validator";
+
+const businessProfileSchema = new mongoose.Schema({
+    owner : {type : String, required : true, index : true},
+    businessName : {type : String, required : true},
+    email : {type : String, required : false, trim : true, lowercase : true, default : ""},
+    address : {type : String, required : false, default : ""},
+    phone : {type : String, required : false, default : ""},
+    gst : {type : String, required : false, default : ""},
+
+    //for image
+    logoUrl : {type : String, required : false, default : null},
+    stampUrl : {type : String, required : false, default : null},
+    signatureUrl : {type : String, required : false, default : null},
+
+    signatureOwnerName : {type : String, required : false, default : ""},
+    signatureOwnerTitle : {type : String, required : false, default : ""},
+
+    defaultTaxPercent : {type : Number, required : false, default : 18},
+},{
+    timestamps : true
+});
+
+const BusinessProfile = mongoose.models.businessName || mongoose.model("BusinessProfile", businessProfileSchema)
+
+export default BusinessProfile;
---
+++ backend/routes/invoiceRouter.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { clerkMiddleware } from '@clerk/express'
+import { createInvoice, deleteInvoice, getInvoices, getInvoideById, updateInvoice } from '../controllers/invoiceController.js';
+
+
+const invoiceRouter = express.Router();
+invoiceRouter.use(clerkMiddleware());
+
+invoiceRouter.get("/", getInvoices);
+invoiceRouter.get("/:id", getInvoideById);
+invoiceRouter.post("/", createInvoice);
+invoiceRouter.put("/:id", updateInvoice);
+invoiceRouter.delete("/:id", deleteInvoice);
+
+export default invoiceRouter;
